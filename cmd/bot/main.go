1b84abbbe63a866225783206edf0d859d5ef937c
Thomas Cooper <thomascooperca@gmail.com>
2022-09-24 17:06:43 -0400

    call twitch init only

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	t "github.com/coopernetes/botrnetes/pkg/twitch"
 )
 
-const (
-	twitchId     = "TWITCH_CLIENT_ID"
-	twitchSecret = "TWITCH_CLIENT_SECRET"
-)
-
 func main() {
-	id := lookup(twitchId)
-	secret := lookup(twitchSecret)
-	t.Init(id, secret)
-}
-
-func lookup(envVar string) string {
-	o, set := os.LookupEnv(envVar)
-	if !set {
-		log.Fatalf("%s is unset!", envVar)
-	}
-	if o == "" {
-		log.Fatalf("%s is empty!", envVar)
-	}
-	return o
+	t.Init()
 }
